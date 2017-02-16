e3b863485dc18f15f7bb094eaca75a37f3282186
colorful <657571285@qq.com>
2017-02-16 15:59:16 +0800

    chat聊天界面的输入框修改

--- app/src/main/java/com/colorful/mqq/activity/ChatActivity.java
+++ app/src/main/java/com/colorful/mqq/activity/ChatActivity.java
@@ -2,6 +2,7 @@ package com.colorful.mqq.activity;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.os.Handler;
 import android.text.TextUtils;
@@ -13,15 +14,21 @@ import android.widget.TextView;
 import com.colorful.mqq.R;
 import com.colorful.mqq.adapter.ChatAdapter;
 import com.colorful.mqq.smack.SmackManager;
+import com.colorful.mqq.ui.Chatkeyboard;
 import com.colorful.mqq.ui.TopTitle;
 import com.colorful.mqq.util.DateUtil;
+import com.colorful.mqq.util.ValueUtil;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
 
 import org.jivesoftware.smack.SmackException;
 import org.jivesoftware.smack.chat.Chat;
 import org.jivesoftware.smack.chat.ChatManagerListener;
 import org.jivesoftware.smack.chat.ChatMessageListener;
 import org.jivesoftware.smack.packet.Message;
+import org.jivesoftware.smackx.filetransfer.FileTransfer;
+import org.jivesoftware.smackx.filetransfer.OutgoingFileTransfer;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -33,7 +40,7 @@ import butterknife.OnClick;
 /**
  * Created by colorful on 2016/12/20.
  */
-public class ChatActivity extends Activity {
+public class ChatActivity extends Activity implements Chatkeyboard.ChatKeyboardOperateListener{
 
     /**
      * 聊天框头部
@@ -80,6 +87,10 @@ public class ChatActivity extends Activity {
      * 数据适配
      */
     private ChatAdapter mAdapter;
+    /**
+     * imageloader加载参数配置
+     */
+    private DisplayImageOptions options;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -109,8 +120,18 @@ public class ChatActivity extends Activity {
         //创建聊天窗口
         chat = SmackManager.getInstance().createChat(chatJid);
 
+        options = new DisplayImageOptions.Builder()
+                .cacheOnDisk(true)//图片下载是否缓存到sdcard
+                .cacheInMemory(true)//图片下载是否缓存到内存
+                .bitmapConfig(Bitmap.Config.RGB_565)//图片解码类型，
+                .considerExifParams(true)
+                .resetViewBeforeLoading(true)
+                .showImageOnFail(R.drawable.pic_default)
+                .showImageOnLoading(R.drawable.pic_default)
+        .build();
+
         List<com.colorful.mqq.bean.Message> list = new ArrayList<>();
-        mAdapter = new ChatAdapter(ChatActivity.this, list);
+        mAdapter = new ChatAdapter(ChatActivity.this, options, list);
         lvChatContent.setAdapter(mAdapter);
     }
 
@@ -141,6 +162,7 @@ public class ChatActivity extends Activity {
     };
 
 
+
     @SuppressLint("HandlerLeak")
     private Handler handler = new Handler() {
         @Override
@@ -153,12 +175,89 @@ public class ChatActivity extends Activity {
                     mAdapter.add((com.colorful.mqq.bean.Message) msg.obj);
                     break;
                 case 2:
+                    mAdapter.update((com.colorful.mqq.bean.Message) msg.obj);
                     break;
 
             }
         }
     };
 
+    /**
+     * 发送消息
+     * @param message
+     */
+    @Override
+    public void send(final String message) {
+        if (ValueUtil.isEmpty(message)){
+            return;
+        }
+        new Thread(){
+            @Override
+            public void run() {
+                try {
+                    chat.sendMessage(message);
+                    com.colorful.mqq.bean.Message msg = new com.colorful.mqq.bean.Message(com.colorful.mqq.bean.Message.MESSAGE_TYPE_TEXT,currNickname,DateUtil.formatDatetime(new Date()),true);
+                    msg.setContent(message);
+                    handler.obtainMessage(1,msg).sendToTarget();
+                } catch (SmackException.NotConnectedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+    }
+
+    /**
+     * 发送文件
+     * @param file
+     * @param type
+     */
+    public void sendFile(File file,int type){
+        OutgoingFileTransfer transfer  = SmackManager.getInstance().getSendFileTransfer(sendUser);
+        try {
+            transfer.sendFile(file,String.valueOf(type));
+            checkTransferStatus(transfer, file, type, true);//检查发送文件的状态
+        } catch (SmackException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**\
+     * 检查发送文件，接受文件的状态
+     * @param transfer
+     * @param file  发送或接收的文件
+     * @param type  文件类型： 图片，文本，语音
+     * @param isSend  是否为发送
+     */
+    private void checkTransferStatus(FileTransfer transfer,File file, int type, boolean isSend) {
+        String username = friendNickname;
+        if(isSend){
+            username = currNickname;
+        }
+        String name = username;
+        com.colorful.mqq.bean.Message msg = new com.colorful.mqq.bean.Message(type,name,DateUtil.formatDatetime(new Date()),isSend);
+
+
+    }
+
+    @Override
+    public void sendVoice(File audioFile) {
+
+    }
+
+    @Override
+    public void recordStart() {
+
+    }
+
+    @Override
+    public void functionClick(int index) {
+
+    }
+
+
+    /**
+     * 发送按钮点击事件
+     *//*
     @OnClick(R.id.bt_send)
     public void onClick() {
         final String message = etSendContent.getText().toString();
@@ -182,5 +281,5 @@ public class ChatActivity extends Activity {
 
             }
         }.start();
-    }
+    }*/
 }
--- app/src/main/java/com/colorful/mqq/adapter/ChatAdapter.java
+++ app/src/main/java/com/colorful/mqq/adapter/ChatAdapter.java
@@ -1,13 +1,22 @@
 package com.colorful.mqq.adapter;
 
 import android.app.Activity;
+import android.graphics.drawable.AnimationDrawable;
+import android.media.MediaPlayer;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroupOverlay;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.colorful.mqq.R;
 import com.colorful.mqq.bean.Message;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
+
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -19,20 +28,33 @@ public class ChatAdapter extends BaseAdapter {
 		public TextView chatUsername;//消息来源人昵称
 		public TextView chatContentTime;//消息时间
 		public TextView chatContentText;//文本消息
+		public ImageView chatUserAvatar;//用户头像
+		public ImageView chatContentImage;//图片消息
+		public ImageView chatContentVoice;//语音消息
+		public ImageView chatContentLoading;//接受文件时的进度条
 	}
 	/**
 	 * 上下文
 	 */
 	private Activity context;
 	/**
+	 * imageloader图片加载配置
+	 */
+	private DisplayImageOptions options;
+	/**
 	 * 聊天数据
 	 */
 	private List<Message> list;
+	/**
+	 * 音频播放器
+	 */
+	private MediaPlayer mediaPlayer;
 
 
-	public ChatAdapter(Activity context, List<Message> list) {
+	public ChatAdapter(Activity context, DisplayImageOptions options, List<Message> list) {
 		super();
 		this.context = context;
+		this.options = options;
 		this.list = list;
 	}
 
@@ -90,16 +112,132 @@ public class ChatAdapter extends BaseAdapter {
 			viewHolder.chatUsername = (TextView) convertView.findViewById(R.id.tv_chat_msg_username);
 			viewHolder.chatContentTime = (TextView) convertView.findViewById(R.id.tv_chat_msg_time);
 			viewHolder.chatContentText = (TextView) convertView.findViewById(R.id.tv_chat_msg_content_text);
-			convertView.setTag(viewHolder);
+			viewHolder.chatUserAvatar = (ImageView) convertView.findViewById(R.id.iv_chat_avatar);
+			viewHolder.chatContentImage = (ImageView) convertView.findViewById(R.id.iv_chat_msg_content_image);
+			viewHolder.chatContentVoice = (ImageView) convertView.findViewById(R.id.iv_chat_msg_content_voice);
+			viewHolder.chatContentLoading = (ImageView) convertView.findViewById(R.id.iv_chat_msg_content_loading);
+				convertView.setTag(viewHolder);
 		} else {
 			viewHolder = (ChatViewHolder) convertView.getTag();
 		}
 		
 		viewHolder.chatUsername.setText(message.getUsername());
 		viewHolder.chatContentTime.setText(message.getDatetime());
-		viewHolder.chatContentText.setText(message.getContent());
+		setMessageViewVisible(message.getType(),viewHolder);
+		if(message.getType() == Message.MESSAGE_TYPE_TEXT){//获取的类型是文本消息
+			viewHolder.chatContentText.setText(message.getContent());
+		}else if (message.getType()==Message.MESSAGE_TYPE_IMAGE){//获取的类型是图片消息
+			String url = "file://"+message.getFilePath();
+			ImageLoader.getInstance().displayImage(url,viewHolder.chatContentImage,options,new SimpleImageLoadingListener());
+			showLoading(viewHolder,message);
+		}else if (message.getType()==Message.MESSAGE_TYPE_VOICE){//获取的类型是语音消息
+			viewHolder.chatContentVoice.setOnClickListener(new View.OnClickListener() {
+				@Override
+				public void onClick(View v) {
+					palyVoice(viewHolder.chatContentVoice,message);
+				}
+			});
+		}
 
 		return convertView;
+	}
+
+	private void palyVoice(final ImageView iv, final Message message) {
+		if(message.isSend()){
+			iv.setBackgroundResource(R.drawable.anim_chat_voice_right);
+		}else {
+			iv.setBackgroundResource(R.drawable.anim_chat_voice_left);
+		}
+		final AnimationDrawable animationDrable = (AnimationDrawable) iv.getBackground();
+		iv.post(new Runnable() {
+			@Override
+			public void run() {
+				animationDrable.start();
+			}
+		});
+		if (mediaPlayer == null || !mediaPlayer.isPlaying()){//状态：未播放。 点击播放，再次点击停止播放
+			//开始播放录音
+			mediaPlayer = new MediaPlayer();
+			mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+				@Override
+				public void onCompletion(MediaPlayer mp) {
+					animationDrable.stop();
+					//恢复语音标志信息图标
+					if (message.isSend()){
+						iv.setBackgroundResource(R.drawable.gxu);
+					}else {
+						iv.setBackgroundResource(R.drawable.gxx);
+					}
+				}
+			});
+
+			try {
+				mediaPlayer.reset();
+				mediaPlayer.setDataSource(message.getFilePath());
+				mediaPlayer.prepare();
+				mediaPlayer.start();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}else {
+			animationDrable.stop();
+			//恢复语音标志信息图标
+			if (message.isSend()){
+				iv.setBackgroundResource(R.drawable.gxu);
+			}else {
+				iv.setBackgroundResource(R.drawable.gxx);
+			}
+			if(mediaPlayer!=null){
+				mediaPlayer.stop();
+				mediaPlayer.release();
+				mediaPlayer = null;
+			}
+		}
+	}
+
+	private void showLoading(ChatViewHolder viewHolder, Message message) {
+		switch (message.getLoadState()){
+			case 0://加载开始
+				viewHolder.chatContentLoading.setBackgroundResource(R.drawable.chat_file_content_loading_anim);
+				final AnimationDrawable animationDrawable = (AnimationDrawable) viewHolder.chatContentLoading.getBackground();
+				viewHolder.chatContentLoading.post(new Runnable() {
+					@Override
+					public void run() {
+						animationDrawable.start();
+					}
+				});
+				viewHolder.chatContentLoading.setVisibility(View.VISIBLE);
+				break;
+			case 1:
+				viewHolder.chatContentLoading.setVisibility(View.GONE);
+				break;
+			case -1:
+				viewHolder.chatContentLoading.setBackgroundResource(R.drawable.load_fail);
+				break;
+
+		}
+	}
+
+	/**
+	 * 根据消息类型展示对应的消息
+	 * @param type
+	 * @param viewHolder
+     */
+	private void setMessageViewVisible(int type,ChatViewHolder viewHolder) {
+		if(type == Message.MESSAGE_TYPE_TEXT){
+			//文本消息
+			viewHolder.chatContentText.setVisibility(View.VISIBLE);
+			viewHolder.chatContentImage.setVisibility(View.GONE);
+			viewHolder.chatContentVoice.setVisibility(View.GONE);
+		}else if(type == Message.MESSAGE_TYPE_IMAGE){
+			viewHolder.chatContentText.setVisibility(View.GONE);
+			viewHolder.chatContentImage.setVisibility(View.VISIBLE);
+			viewHolder.chatContentVoice.setVisibility(View.GONE);
+		}else if (type == Message.MESSAGE_TYPE_VOICE){
+			viewHolder.chatContentText.setVisibility(View.GONE);
+			viewHolder.chatContentImage.setVisibility(View.GONE);
+			viewHolder.chatContentVoice.setVisibility(View.VISIBLE);
+		}
 	}
 
 }
